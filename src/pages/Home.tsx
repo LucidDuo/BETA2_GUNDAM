a102258373eb5bb139d048aa92f115cb5ae0cb48
kimyoojun <anano3134@gmail.com>
2026-09-13 16:42:04 +0900

    영상 변환

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -236,7 +236,7 @@ export default function Home() {
             }}
           >
             
-            <source src="/fake.mp4" type="video/mp4" />
+            <source src="/gundamtiger.mp4" type="video/mp4" />
           </video>
           {/* Dark overlay */}
           <div
